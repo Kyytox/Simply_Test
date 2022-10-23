85f30c8f9a3ad39a4233f708d93b2b6616e6d87b
Kyytox <thomas.caillault@protonmail.com>
2022-10-23 08:00:17 +0200

    add AOS lib

--- frontend/src/home/home.js
+++ frontend/src/home/home.js
@@ -2,20 +2,31 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import { NavLink } from "react-router-dom";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import "./home.css";
 
+
+AOS.init({
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 150, // values from 0 to 3000, with step 50ms
+    duration: 1000, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them  
+});
 
 function Home() {
     return (
         <div className="main-home">
 
-            <div className="home-title">
+            <div className="home-title" data-aos="fade-down">
                 <h1>Automate your tests</h1>
                 <h1>Verify your design</h1>
             </div>
 
             <div className="home-description">
-                <div className="div-apps">
+                <div className="div-apps" data-aos="fade-up-right">
                     <div className="app-expli">
                         <p>Automate your tests by creating individual test</p>
                     </div>
@@ -25,7 +36,7 @@ function Home() {
                         </NavLink>
                     </div>
                 </div>
-                <div className="div-apps">
+                <div className="div-apps" data-aos="fade-up-left">
                     <div className="app-expli">
                         <p>Check the compatibility of your design on different browsers</p>
                     </div>
@@ -37,7 +48,7 @@ function Home() {
                 </div>
             </div>
 
-            <div className="div-boxs-apps">
+            <div className="div-boxs-apps" data-aos="fade-right">
                 <div className="box-app">
                     <p>Automate your tests by creating individual action</p>
                     <p>Automate your tests by creating individual action</p>
@@ -45,16 +56,19 @@ function Home() {
                     <p>Automate your tests by creating individual action</p>
                 </div>
 
-                <div className="box-app">
+                <div className="box-app" data-aos="fade-left">
                     <p>Check the compatibility of your design on different browsers</p>
                     <p>Check the compatibility of your design on different browsers</p>
                     <p>Check the compatibility of your design on different browsers</p>
                     <p>Check the compatibility of your design on different browsers</p>
                 </div>
-
             </div>
 
 
+            <div className="footer">
+                <hr></hr>
+                <p>Made by Kytox</p>
+            </div>
 
         </div >
     );
